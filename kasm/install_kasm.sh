78ad4058982d6ad2b15a012c93b1c75845b102dd
cmndcntrlcyber <r.soreng@c3s.consulting>
2024-11-24 11:26:32 -0800

    modified files

--- kasm/install_kasm.sh
+++ kasm/install_kasm.sh
@@ -1,8 +1,29 @@
-#!/bin/sh
-ACCEPT_EULA=y
+## Install Kasm
+# Set up environment for Kasm installation
 cd /tmp
-curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_1.16.1.98d6fa.tar.gz
-curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_service_images_amd64_1.16.1.98d6fa.tar.gz
-curl -O https://kasm-static-content.s3.amazonaws.com/kasm_release_workspace_images_amd64_1.16.1.98d6fa.tar.gz
-tar -xf kasm_release_1.16.1.98d6fa.tar.gz
-sudo echo $ACCEPT_EULA | bash kasm_release/install.sh --role db --database-user reactor --database-name btpi_react --offline-workspaces /tmp/kasm_release_workspace_images_amd64_1.16.1.98d6fa.tar.gz --offline-service /tmp/kasm_release_service_images_amd64_1.16.1.98d6fa.tar.gz
+
+# Add necessary tools
+apk add bash tar
+
+# Download required files
+curl -fLO https://kasm-static-content.s3.amazonaws.com/kasm_release_1.15.0.06fdc8.tar.gz
+curl -fLO https://kasm-static-content.s3.amazonaws.com/kasm_release_workspace_images_amd64_1.15.0.06fdc8.tar.gz
+curl -fLO https://kasm-static-content.s3.amazonaws.com/kasm_release_service_images_amd64_1.15.0.06fdc8.tar.gz
+
+# Verify downloads
+sha256sum /tmp/*.tar.gz
+
+# Extract release tarball
+tar -xf kasm_release_1.15.0.06fdc8.tar.gz
+
+# Debugging: Check the directory structure
+ls -la ./kasm_release/
+
+# Ensure the installer script is executable
+chmod +x ./kasm_release/install.sh
+
+# Set the environment variable to accept the EULA
+ACCEPT_EULA=y
+
+# Run the installation script with the necessary parameters
+echo $ACCEPT_EULA | bash -x ./kasm_release/install.sh --offline-workspaces /tmp/kasm_release_workspace_images_amd64_1.15.0.06fdc8.tar.gz --offline-service /tmp/kasm_release_service_images_amd64_1.15.0.06fdc8.tar.gz
