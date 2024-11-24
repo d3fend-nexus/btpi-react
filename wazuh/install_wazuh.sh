bb8e12f7a05eeb6cf7c75fe20e24d6f6bfdcbd08
cmndcntrlcyber <r.soreng@c3s.consulting>
2024-11-24 12:18:10 -0800

    updated file

--- wazuh/install_wazuh.sh
+++ wazuh/install_wazuh.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/wazuh/wazuh-docker.git -b v4.9.2
 cd /opt/wazuh-docker/multi-node
 rm docker-compose.yaml
-curl https://raw.githubusercontent.com/cmndcntrlcyber/btpi-react/refs/heads/main/grr/compose.yaml > /opt/grr/compose.yaml
+curl https://raw.githubusercontent.com/cmndcntrlcyber/btpi-react/refs/heads/main/wazuh/docker-compose.yml > /opt/wazuh-docker/multi-node/docker-compose.yml
 docker compose -f generate-indexer-certs.yml run --rm generator
 docker compose up -d
